21ff159ec27c7c94c74f1776d6687e50f66239d8
Utkarsh Morey <utkarshmorey0705@gmail.com>
2025-11-17 16:07:53 +0530

    Update product.c

--- product.c
+++ product.c
@@ -5,10 +5,11 @@ struct product
     char name[10];
     int IDno;
     int price;
+    int quantity;
 };
 
 int main()
-{
+  {
     int m, n;
     printf("Enter number of products: ");
     scanf("%d", &n);
@@ -16,7 +17,6 @@ int main()
 
     struct product P[15];
     
-    // Input product details
     for (m = 0; m < n; m++)
     {
         printf("Enter Product Name: ");
@@ -28,9 +28,12 @@ int main()
         printf("Enter Product Price: ₹");
         scanf("%d", &P[m].price);
         printf("\n");
+
+         printf("Enter Product Quantity: ");
+        scanf("%d", &P[m].quantity);
+        printf("\n");
     }
     
-    // Sorting products in descending order of price
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
@@ -44,12 +47,25 @@ int main()
         }
     }
 
-    // Display sorted list
+        for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (P[j].quantity < P[j + 1].quantity)
+            {
+                struct product temp = P[j];
+                P[j] = P[j + 1];
+                P[j + 1] = temp;
+            }
+        }
+    }
+
+
     printf("\nThe list of the products in descending order of price is:\n");
     printf("--------------------------------------------------------\n");
     for (m = 0; m < n; m++)
     {
-        printf("Name: %s | ID no.: %d | Price: ₹%d\n", P[m].name, P[m].IDno, P[m].price);
+        printf("Name: %s | ID no.: %d | Price: ₹%d | Quantity: %d \n", P[m].name, P[m].IDno, P[m].price, P[m].quantity);
     }
 
     return 0;
