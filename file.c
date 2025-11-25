8ba885e5216821aae89a6947d0ba0a2ba413015d
Utkarsh Morey <utkarshmorey0705@gmail.com>
2025-11-25 22:57:47 +0530

    Update file.c

--- file.c
+++ file.c
@@ -5,8 +5,10 @@ int main()
     FILE *fptr;
     
     int menu;
-    printf("Menu:\n1.Enter candidate name\n2.Show the list of the candidate\n3.Exit\nOption:");
+    printf("Menu:\n1.Enter candidate Information\n2.Show the list of the candidate\n3.Exit\n4.Delete the file\nOption:");
     scanf("%d",&menu);
+    printf("\n");
+
     
     switch(menu)
     {
@@ -15,25 +17,34 @@ int main()
         int num;
         printf("Number fo candidates:");
         scanf("%d",&num);
+        printf("\n");
+
         
         fptr=fopen("candidate.txt","a");
         char name[10];
-        printf("Enter candidates name(Give space at end):\n");
-        
+        int age,serialno;
+
         for(int i=0;i<num;i++)
         {
+          printf("Enter candidates name(Give space at end):");
           scanf(" %49[^\n]",&name);
-          fprintf(fptr,"%s",name);
+          printf("Enter age:");
+          scanf("%d",&age);
+          printf("Enter serial no.:");
+          scanf("%d",&serialno);
+          printf("\n");
+
+          fprintf(fptr,"Name:%s\nAge:%d\nserial no.:%d\n\n",name,age,serialno);  
         }
         fclose(fptr);
-        printf("Name saved to file...");
+        printf("Information saved to file...");
         break;
         
         case 2:
         
         fptr=fopen("candidate.txt","r");
     
-        printf("\n\nList of the candidate is:  ");
+        printf("\n\nList of the candidate is: \n\n");
         char ch;
         while((ch=fgetc(fptr))!=EOF)
         {
@@ -46,6 +57,13 @@ int main()
         
         printf("Exititng the program....");
         break;
+        
+        case 4:
+        
+        fptr=fopen("candidate.txt","w");
+        fclose(fptr);
+        printf("Deleting the file...");
+        
     }
 
     return 0;
